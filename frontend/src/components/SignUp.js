91109e9d989eeae08bb22b0a0a3bfd49dc4062e1
tirthasaha36 <tirtha.saha366@gmil.com>
2025-05-22 13:45:05 +0530

    Update social media buttons in SignIn and SignUp components with icons

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -82,11 +82,17 @@ const SignUp = ({ onBack, onSignIn }) => {
           <button onClick={handleNext} style={styles.button}>Sign Up</button>
           <p style={styles.or}>or</p>
           <div style={styles.socialButtons}>
-            <button style={styles.socialButton}>G</button>
-            <button style={styles.socialButton}>F</button>
-            <button style={styles.socialButton}>O</button>
+            <button style={styles.socialButton}>
+              <img src="https://img.icons8.com/?size=100&id=17949&format=png&color=000000" alt="Google" style={styles.socialIcon} />
+            </button>
+            <button style={styles.socialButton}>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_(2019).png" alt="Facebook" style={styles.socialIcon} />
+            </button>
+            <button style={styles.socialButton}>
+              <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="Apple" style={{...styles.socialIcon, objectFit: 'contain'}} />
+            </button>
           </div>
-          <p>
+          <p style={styles.centeredText}>
             Already have an account?{' '}
             <span style={styles.link} onClick={onSignIn}>Sign in</span>
           </p>
@@ -186,18 +192,28 @@ const styles = {
   },
   socialButtons: {
     display: 'flex',
-    justifyContent: 'space-around',
+    justifyContent: 'center',
+    gap: 5,
     marginBottom: 15,
   },
   socialButton: {
     backgroundColor: '#eee',
     border: 'none',
-    padding: 10,
-    fontSize: 18,
+    padding: 5,
     cursor: 'pointer',
     width: 50,
     height: 50,
     borderRadius: 25,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  socialIcon: {
+    width: 24,
+    height: 24,
+  },
+  centeredText: {
+    textAlign: 'center',
   },
   link: {
     color: '#FFC107',
