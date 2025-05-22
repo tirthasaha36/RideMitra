faaf32bd8913acb92706b791d0e609d58901165e
Tirtha Saha <tirtha.saha366@gmail.com>
2025-05-22 18:45:30 +0530

    Updated the logo

--- frontend/src/components/SplashScreen.js
+++ frontend/src/components/SplashScreen.js
@@ -1,28 +1,21 @@
 import React from 'react';
+import RideMitraLogo from '../assets/images/RideMitra Logo.png';
 
 const SplashScreen = ({ onNext }) => {
   return (
     <div style={styles.container}>
       <div style={styles.logoContainer}>
         <div style={styles.logoCircle}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="64"
-            height="64"
-            fill="white"
-            viewBox="0 0 24 24"
-          >
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8 0-1.85.63-3.55 1.69-4.9l10.21 10.21c-1.35 1.06-3.05 1.69-4.9 1.69zm6.31-2.1L8.1 7.69c1.35-1.06 3.05-1.69 4.9-1.69 4.41 0 8 3.59 8 8 0 1.85-.63 3.55-1.69 4.9z" />
-          </svg>
+          <img src={RideMitraLogo} alt="RideMitra Logo" style={styles.logoImage} />
         </div>
-        <h1 style={styles.title}>Easy Rider</h1>
+        <h1 style={styles.title}>RideMitra</h1>
       </div>
       <div style={styles.loader}>
         <div className="loader"></div>
       </div>
-<button style={styles.nextButton} onClick={onNext} aria-label="Next">
-  {'>'}
-</button>
+      <button style={styles.nextButton} onClick={onNext} aria-label="Next">
+        {'>'}
+      </button>
     </div>
   );
 };
@@ -50,6 +43,11 @@ const styles = {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  logoImage: {
+    width: 96,
+    height: 96,
+    objectFit: 'contain',
   },
   title: {
     color: 'white',
